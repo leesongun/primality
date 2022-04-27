58f7093b8980eddcb05772a32ca291cdc5b76520
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-28 05:49:28 +0900

    implement fermat prime test(not yet sprp)

---
+++ julia/sprp.jl
@@ -0,0 +1,14 @@
+using oneAPI
+N = 0x800_0000
+#UInt32 is enough actually
+#UInt32 would allow 2x more entries
+p = oneArray{UInt64}(3:2:N*2+1)
+t = oneArray(ones(UInt64, N))
+for i in 31:-1:1
+    j = UInt64(1) << i
+    t .*= (((p .& j) .!= 0) .+ 1)
+    t .*= t;
+    t .%= p;
+    print("$i\n")
+end
+t .== 1
