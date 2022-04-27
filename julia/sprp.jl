6726def9ecdaf4cdb45994d7b8831856a125da5b
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-28 05:53:38 +0900

    add comment on future direction

--- julia/sprp.jl
+++ julia/sprp.jl
@@ -2,6 +2,7 @@ using oneAPI
 N = 0x800_0000
 #UInt32 is enough actually
 #UInt32 would allow 2x more entries
+#better to check 4k+3, 8k+5, ... order?
 p = oneArray{UInt64}(3:2:N*2+1)
 t = oneArray(ones(UInt64, N))
 for i in 31:-1:1
